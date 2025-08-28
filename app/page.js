c5d6f044f754c0457d2a387033fc4870b2d8522f
Umar Farooq <uf29664@gmail.com>
2025-08-28 21:46:08 +0500

    home

--- app/page.js
+++ app/page.js
@@ -6,11 +6,14 @@ export default function Home() {
    <div className="bg-[#2b2b2b] h-screen min-w-full z-0  text-white">
     <Navbar/>
 
-    <div className="px-[125px] py-[80px] grid grid-cols-1 lg:grid-cols-2 gap-[30px] bg-red-400">
+    <div className="px-[125px] py-[80px] grid grid-cols-1 lg:grid-cols-2 gap-[45px] ">
       <div className="">
-        <h1 className="text-[87px] font-[600]   ">Discover <span className=""><br className='hidden lg:block'/></span> Digital Art & <span className=""><br className='hidden lg:block'/></span> Collect NFTs</h1>
-        <p className="text-2xl ">NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+        <h1 className="text-[28px] md:text-[38px] lg:text-[68px] font-[600]   ">Discover <span className=""></span> Digital Art & <span className=""></span> Collect NFTs</h1>
+        <p className="text-[22px] font-[400] pt-3">NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+        <div className="block">
+
         <Button className='bg-[#A259FF] h-[60px] w-[224px] ' img='RocketLaunch.svg'  title='Get Started'/>
+        </div>
         <div>
           <div className="">
             <h4 className="">240k+</h4>
@@ -20,7 +23,7 @@ export default function Home() {
         </div>
       </div>
       <div className="">
-        <Image src='/space.png' width={510} height={401} alt="space man Image" />
+        <Image className='w-full object-cover rounded-xl  h-[401px]' src='/space.png' width={510} height={401} alt="space man Image" />
         <h1 className="">Space Walking</h1>
         <div className="">
             <Image src='/smallicon.svg' width={24} height={24} alt="space man Image" />

548c564b59b59559e0618447daa45c5f43b9d97d
Umar-Farooq-217 <uf29664@gmail.com>
2025-09-04 15:21:37 +0500

    card

---
+++ app/components/card/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+      <Image className='' width={} height={} alt='' />
+      
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-    <div className=""></div>
+   
     
     </div>
   );

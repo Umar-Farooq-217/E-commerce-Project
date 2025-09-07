cc4e1564fd699bf9c5dc1eb1eababded5996dd99
Umar-Farooq-217 <uf29664@gmail.com>
2025-09-07 22:07:40 +0500

    third container

--- app/components/card/Card.js
+++ app/components/card/Card.js
@@ -4,11 +4,11 @@ import React from 'react'
 export default function Card(props) {
   return (
     <div>
-      <Image className='h-[330px]  w-full rounded-xl  ' src={props.first} width={100} height={100} alt='Image' />
+      <Image className='h-[330px]  w-full rounded-xl   [image-rendering:smooth]' src={props.first} width={100} height={100} alt='Image' />
       <div className="flex justify-between gap-4 mt-3">
 <Image className='' src={props.second} width={100} height={100} alt='Image' />
 <Image className='' src={props.third} width={100} height={100} alt='Image' />
-<h1 className='bg-[#A259FF] h-[100px] w-[100px] text-[22px] font-bold flex justify-center items-center rounded-xl  '>1025+</h1>
+<h1 className='bg-[#A259FF] h-[100px] w-[100px] text-[22px] font-bold flex justify-center items-center rounded-2xl  '>1025+</h1>
 
       </div>
       <h1 className="text-[22px] font-semibold pt-2 ">{props.heading}</h1>
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="bg-[#2b2b2b]  min-w-full z-0  text-white">
       <Navbar />
 <div className="px-[125px] py-[80px]">
-      <div className=" grid grid-cols-1 lg:grid-cols-2 gap-[45px] ">
+      <div className=" grid grid-cols-1 lg:grid-cols-2 gap-[45px] py-7 ">
         <div className="p-2">
           <h1 className="text-[28px] md:text-[38px] lg:text-[68px] font-[600]  leading-tight text-balance ">Discover <span className=""><br className='hidden lg:block' /></span> Digital Art & <span className=""><br className='hidden lg:block' /></span> Collect NFTs</h1>
           <p className="text-[22px] font-[400] pt-3">NFT marketplace UI created with Anima for Figma. <br className='hidden lg:block' />Collect, buy and sell art from more than 20k NFT artists.</p>
@@ -45,13 +45,22 @@ export default function Home() {
       </div>
       {/* ///////////////////////////////////// */}
       <div className="py-[80px]">
-        <h1 className="text-[28px] lg:text-[38px] font-[600] ">Trending Collections</h1>
+        <h1 className="text-[28px] lg:text-[38px] font-[600] pt-7">Trending Collections</h1>
         <p className="text-[16px] lg:text-[22px] font-[400] ">Checkout our Weekly Updated Trending Collection</p>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-16">
           <Card first='/Primary Photo Placeholder.png' second='/Secondary Photo Placeholder.png' third='/Secondary Photo Placeholder-1.png' four='/Avatar Placeholder.png' heading='DSGN Animals' pera='MrFox' />
           <Card first='/Primary Photo Placeholder-1.png' second='/Secondary Photo Placeholder-2.png' third='/Secondary Photo Placeholder-3.png' four='/Avatar Placeholder-1.png' heading='Magic Mashrooms' pera='Shroomie' />
           <Card first='/Primary Photo Placeholder-2.png' second='/Secondary Photo Placeholder-4.png' third='/Secondary Photo Placeholder-5.png' four='/Avatar Placeholder-2.png' heading='Disco Machine' pera='Bekind2Robots' />
         </div>
+        </div>
+
+        {/* 3rd///////////////////////////////// */}
+        <div className="">
+            <h1 className="text-[28px] lg:text-[38px] font-[600] pt-7">Top Creators</h1>
+            <div className="">
+        <p className="text-[16px] lg:text-[22px] font-[400] ">Checkout our Weekly Updated Trending Collection</p>
+        <Button />
+            </div>
         </div>
    
     </div>

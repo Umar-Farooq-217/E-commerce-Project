ecdc239ad228b6248c0937c3f7d9d7bad89657dd
Umar-Farooq-217 <uf29664@gmail.com>
2025-10-26 19:42:32 +0500

    page

--- app/createAccount/page.js
+++ app/createAccount/page.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
-
+import Footer from '../components/footer'
 export default function page() {
   return (
     <div className=''>
         <Navbar/>
+        <div className="">
+          
+        </div>
+
+        <Footer/>
       
     </div>
   )
